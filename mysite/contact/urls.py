99880cd91326d96c4878e06720ab13b39099b26c
rajusah-works <sahofficialworks@gmail.com>
2024-07-02 20:56:05 +0530

    Delete mysite directory

--- mysite/contact/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import contact_view, success_view
-urlpatterns = [
-    path('', contact_view, name='contact'),
-    path('success/', success_view, name='success'),
-]
